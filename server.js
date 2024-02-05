a7fd82768b0fb95c29ed4723ad8feaf98e03c0cc
Ishan9898 <129816884+Ishan9898@users.noreply.github.com>
2024-02-06 00:32:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// const express = require('express')
+// const colors = require('colors') 
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
@@ -5,45 +7,55 @@ import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
-import productRoutes from "./routes/productRoute.js";
+import productRoutes from "./routes/productRoutes.js"
 import cors from "cors";
 import path from "path";
 import {fileURLToPath} from "url";
 
-//configure env
+
+
+
+// configure env
 dotenv.config();
 
-//databse config
+
+// database config
 connectDB();
 
 const __filename=fileURLToPath(import.meta.url);
 const __dirname=path.dirname(__filename);
 
-//rest object   
-const app = express();     
 
-//middelwares 
-app.use(cors()); 
+
+// rest object
+const app = express();
+
+// middlewares
+app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname,'./client/build')))
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname,'./client/build')));
 
-//routes
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/category", categoryRoutes);
-app.use("/api/v1/product", productRoutes);
+// routes
+app.use('/api/v1/auth',authRoutes);
+app.use('/api/v1/category',categoryRoutes);
+app.use("/api/v1/product",productRoutes);
 
-//rest api  
+// rest api 
 app.use('*',function (req,res) {
     res.sendFile(path.join(__dirname,'./client/build/index.html'));
 })
 
-//PORT
+
+// POST 
+// For react we use port 3000
+// For angular we use port 200
+// For node the most common port is 8000 or 8080
+
 const PORT = process.env.PORT || 8080;
 
-//run listen
-app.listen(PORT, () => {
-    console.log(
-        `Server Running on port ${PORT}`.bgGreen.black
-    );
+
+// run listen
+app.listen(PORT , () =>{
+    console.log(`Server Running on${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
 });
