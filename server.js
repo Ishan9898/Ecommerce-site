73c4a094489513b568bc88c8e479c5601275ac86
Ishan9898 <129816884+Ishan9898@users.noreply.github.com>
2024-02-06 00:09:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.use('/api/v1/category',categoryRoutes);
 app.use("/api/v1/product",productRoutes);
 
 // rest api 
-app.listen(PORT, () => {
-    resizeBy.sendFile(path.join(__dirname,'./client/build/index.html'));
+app.use('*',function (req,res) {
+    res.sendFile(path.join(__dirname,'./client/build/index.html'));
 })
 
 // POST 
